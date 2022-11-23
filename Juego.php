78d820bdc90cea7ea45cd4c6766716e13a709c1c
Antonio <arodbol97@gmail.com>
2022-11-23 20:41:42 +0100

    323

---
+++ Juego.php
@@ -0,0 +1,31 @@
+<?php
+class Juego extends Soporte{
+    public $consola;
+    private $minNumJugadores;
+    private $maxNumJugadores;
+
+    public function __construct($titulo, $numero, $precio, $consola, $minNumJugadores, $maxNumJugadores){
+        parent::__construct($titulo, $numero, $precio);
+        $this->consola=$consola;
+        $this->minNumJugadores=$minNumJugadores;
+        $this->maxNumJugadores=$maxNumJugadores;
+    }
+
+    public function muestraJugadoresPosibles(){
+        if($this->minNumJugadores==$this->maxNumJugadores){
+            if($this->maxNumJugadores==1){
+                echo "Para un Jugador";
+            }else{
+                echo "Para ".$this->maxNumJugadores." jugadores";
+            }
+        }else{
+            echo "De ".$this->minNumJugadores." a ".$this->maxNumJugadores." jugadores";
+        }
+    }
+
+    public function muestraResumen(){
+        echo "<br>";
+        parent::muestraResumen();
+        $this->muestraJugadoresPosibles();
+    }
+}
